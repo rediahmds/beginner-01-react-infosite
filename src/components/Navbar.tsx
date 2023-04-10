1ef149ab664e0d8a0de10c6d2d81130311eb657c
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-10 17:46:43 +0700

    add: Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="React Logo"
+      />
+    </nav>
+  );
+}
