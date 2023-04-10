bca9e6680a38d6e0a4dc5d75063f08cdae51cb38
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-10 22:05:26 +0700

    Add contents

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 export default function Main() {
   return (
     <div>
-      <h1>Fun Facts about React</h1>
+      <h1 className="text-4xl">Fun Facts about React</h1>
       <ul>
-        <li>hhsd</li>
-        <li>gdfjio</li>
-        <li>jrgje</li>
+        <li>First released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Maintained by Facebook (now meta)</li>
+        <li>Powers thousands of enterprise apps, including mobile</li>
       </ul>
     </div>
   );
