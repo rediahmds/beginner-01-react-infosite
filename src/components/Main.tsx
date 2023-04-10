4b261372e83c90812cf85138431c7ca710a5a89b
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-10 17:54:46 +0700

    create component: Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>hhsd</li>
+        <li>gdfjio</li>
+        <li>jrgje</li>
+      </ul>
+    </div>
+  );
+}
