c2e444ba48b150c37df844f53f1c742e50215380
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-11 17:28:12 +0700

    style: create responsive component Main
    Add some styling such as font size, font weight, padding, breakpoints, and text size

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function Main() {
   return (
-    <div>
-      <h1 className="text-4xl">Fun Facts about React</h1>
-      <ul>
+    <div className="py-6">
+      <h1 className="text-xl font-bold leading-10 text-left pl-3 text-slate-200 sm:text-3xl md:pl-5 md:text-5xl">
+        Fun Facts about React
+      </h1>
+      <ul className="pl-10 py-2 list-disc list-outside text-left text-slate-200 sm:text-xl md:pl-16 md:text-2xl">
         <li>First released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
