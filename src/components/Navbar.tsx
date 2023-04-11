1ea6d24f24cb40d98f2efb555711d33f61d5006b
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-11 16:32:14 +0700

    fix: Navbar component
    Fix irresponsive component on smaller mobile phone screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,12 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 flex justify-between py-4">
       <div className="flex">
-        <img src={logo} alt="React Logo" className="md:w-20" />
+        <img src={logo} alt="React Logo" className="w-16 md:w-20" />
         <h2 className="self-center font-bold text-cyan-400 text-sm sm:text-base md:text-xl lg:text-3xl">
           ReactFacts
         </h2>
       </div>
-      <h3 className="self-center font-bold text-sm pr-7 text-slate-200 sm:text-base md:text-xl lg:text-3xl ">
+      <h3 className="self-center font-bold text-xs pr-7 text-slate-200 sm:text-base md:text-xl lg:text-3xl ">
         Project 01: React Info site
       </h3>
     </nav>
